dbe43b12fa50daf7f4298e7522f36d7d5d4826ea
mehdi <you@example.commehdi.zirar@student.hogent.be>
2026-09-24 14:53:15 +0200

    voedingsproduct bugs opgelost

--- Winkelkar/Winkelkar/Model/VoedingsProduct.cs
+++ Winkelkar/Winkelkar/Model/VoedingsProduct.cs
@@ -3,7 +3,7 @@
 public class VoedingsProduct : Product {
     public VoedingsProduct(string omschrijving, double prijsPerStuk, Nutriscore nutriscore)
         : base(omschrijving, prijsPerStuk) {
-
+        _nutriscore = nutriscore;
     }
 
     private Nutriscore _nutriscore;
@@ -18,21 +18,13 @@ public class VoedingsProduct : Product {
     }
 
     public int BepaalKortingsPercentage() {
-        int kortingsPercentage = 0;
-        return kortingsPercentage switch {
-            (int)Nutriscore.A => 30,
-            (int)Nutriscore.B => 25,
-            (int)Nutriscore.C => 15,
-            (int)Nutriscore.D => 5,
-            (int)Nutriscore.E => 0
+        return Nutriscore switch {
+            Nutriscore.A => 30,
+            Nutriscore.B => 25,
+            Nutriscore.C => 15,
+            Nutriscore.D => 5,
+            Nutriscore.E => 0
         };
-        //if (Nutriscore == Nutriscore.A) { kortingsPercentage = 30; }
-        //else if (Nutriscore == Nutriscore.B) { kortingsPercentage = 25; }
-        //else if (Nutriscore == Nutriscore.C) { kortingsPercentage = 15; }
-        //else if (Nutriscore == Nutriscore.D) { kortingsPercentage = 5; }
-        //else { kortingsPercentage = 0; }
-
-        return kortingsPercentage;
     }
 
     public override double GeefPrijsMetKorting() {
--- Winkelkar/Winkelkar/Program.cs
+++ Winkelkar/Winkelkar/Program.cs
@@ -4,13 +4,29 @@ namespace Winkelkar {
     internal class Program {
         static void Main(string[] args) {
             VoedingsProduct product1 = new("Big Mac Deluxe Meal", 100, Nutriscore.A);
-            double prijsMetKorting = product1.GeefPrijsMetKorting();
-            Console.WriteLine($"De prijs zonder korting is {product1.GeefPrijsZonderKorting()}");
-            Console.WriteLine($"De prijs met korting is {prijsMetKorting}");
+            double prijsMetKorting1 = product1.GeefPrijsMetKorting();
+            Console.WriteLine($"De prijs van {product1.Omschrijving} zonder korting is {product1.GeefPrijsZonderKorting()}");
+            Console.WriteLine($"De prijs van {product1.Omschrijving} met korting is {prijsMetKorting1}");
 
-            VoedingsProduct product2 = new("Appel", 100, Nutriscore.A);
-            Console.WriteLine($"De prijs zonder korting is {product2.GeefPrijsZonderKorting()}");
-            Console.WriteLine($"De prijs met korting is {product2.GeefPrijsMetKorting()}");
+            VoedingsProduct product2 = new("Appel", 100, Nutriscore.B);
+            double prijsMetKorting2 = product2.GeefPrijsMetKorting();
+            Console.WriteLine($"De prijs van {product2.Omschrijving} zonder korting is {product2.GeefPrijsZonderKorting()}");
+            Console.WriteLine($"De prijs van {product2.Omschrijving} met korting is {prijsMetKorting2}");
+
+            VoedingsProduct product3 = new("Peer", 100, Nutriscore.C);
+            double prijsMetKorting3 = product3.GeefPrijsMetKorting();
+            Console.WriteLine($"De prijs van {product3.Omschrijving} zonder korting is {product3.GeefPrijsZonderKorting()}");
+            Console.WriteLine($"De prijs van {product3.Omschrijving} met korting is {prijsMetKorting3}");
+
+            VoedingsProduct product4 = new("Peer", 100, Nutriscore.D);
+            double prijsMetKorting4= product4.GeefPrijsMetKorting();
+            Console.WriteLine($"De prijs van {product4.Omschrijving} zonder korting is {product4.GeefPrijsZonderKorting()}");
+            Console.WriteLine($"De prijs van {product4.Omschrijving} met korting is {prijsMetKorting4}");
+
+            VoedingsProduct product5 = new("Peer", 100, Nutriscore.E);
+            double prijsMetKorting5 = product5.GeefPrijsMetKorting();
+            Console.WriteLine($"De prijs van {product5.Omschrijving} zonder korting is {product5.GeefPrijsZonderKorting()}");
+            Console.WriteLine($"De prijs van {product5.Omschrijving} met korting is {prijsMetKorting5}");
         }
     }
 }
