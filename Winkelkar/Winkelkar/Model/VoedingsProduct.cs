fc89819b157a4ecd8553a93b3e57d67e59a843e8
mehdi <you@example.commehdi.zirar@student.hogent.be>
2026-09-24 13:23:47 +0200

    refactoring

--- Winkelkar/Winkelkar/Model/VoedingsProduct.cs
+++ Winkelkar/Winkelkar/Model/VoedingsProduct.cs
@@ -1,6 +1,4 @@
-﻿using System.Security.Cryptography.X509Certificates;
-
-namespace WinkelkarProject.Model;
+﻿namespace WinkelkarProject.Model;
 
 public class VoedingsProduct : Product {
     public VoedingsProduct(string omschrijving, double prijsPerStuk, Nutriscore nutriscore)
@@ -21,17 +19,17 @@ public class VoedingsProduct : Product {
 
     public int BepaalKortingsPercentage() {
         int kortingsPercentage = 0;
-        //return kortingsPercentage switch {
-        //    (int)Nutriscore.A => 30,
-        //    (int)Nutriscore.B => 25,
-        //    (int)Nutriscore.C => 15,
-        //    (int)Nutriscore.D => 5,
-        //    (int)Nutriscore.E => 0
-        //};
-        if (Nutriscore == Nutriscore.A) { kortingsPercentage = 30; }
-        else if (Nutriscore == Nutriscore.B) { kortingsPercentage = 25; }
-        else if (Nutriscore == Nutriscore.C) { kortingsPercentage = 15; }
-        else if (Nutriscore == Nutriscore.D) { kortingsPercentage = 5; }
+        return kortingsPercentage switch {
+            (int)Nutriscore.A => 30,
+            (int)Nutriscore.B => 25,
+            (int)Nutriscore.C => 15,
+            (int)Nutriscore.D => 5,
+            (int)Nutriscore.E => 0
+        };
+        //if (Nutriscore == Nutriscore.A) { kortingsPercentage = 30; }
+        //else if (Nutriscore == Nutriscore.B) { kortingsPercentage = 25; }
+        //else if (Nutriscore == Nutriscore.C) { kortingsPercentage = 15; }
+        //else if (Nutriscore == Nutriscore.D) { kortingsPercentage = 5; }
         //else { kortingsPercentage = 0; }
 
         return kortingsPercentage;
--- Winkelkar/Winkelkar/Program.cs
+++ Winkelkar/Winkelkar/Program.cs
@@ -1,7 +1,16 @@
-﻿namespace Winkelkar {
+﻿using WinkelkarProject.Model;
+
+namespace Winkelkar {
     internal class Program {
         static void Main(string[] args) {
-            Console.WriteLine("Hello, World!");
+            VoedingsProduct product1 = new("Big Mac Deluxe Meal", 100, Nutriscore.A);
+            double prijsMetKorting = product1.GeefPrijsMetKorting();
+            Console.WriteLine($"De prijs zonder korting is {product1.GeefPrijsZonderKorting()}");
+            Console.WriteLine($"De prijs met korting is {prijsMetKorting}");
+
+            VoedingsProduct product2 = new("Appel", 100, Nutriscore.A);
+            Console.WriteLine($"De prijs zonder korting is {product2.GeefPrijsZonderKorting()}");
+            Console.WriteLine($"De prijs met korting is {product2.GeefPrijsMetKorting()}");
         }
     }
 }
