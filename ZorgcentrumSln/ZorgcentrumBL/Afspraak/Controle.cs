b472abf1dcd36b3491ccb4d90cda0c90ec1f622d
mehdi <you@example.commehdi.zirar@student.hogent.be>
2026-09-25 21:45:32 +0200

    contructors aangemaakt van controle en operatie klassen

--- ZorgcentrumSln/ZorgcentrumBL/Afspraak/Controle.cs
+++ ZorgcentrumSln/ZorgcentrumBL/Afspraak/Controle.cs
@@ -4,5 +4,8 @@ using System.Text;
 
 namespace ZorgcentrumBL.Afspraak {
     public class Controle : Afspraak {
+        public Controle(int id, string type, DateTime datumTijdStart, int duurMin, string rijksRegisterNrPatient, string rijksRegisterNrArts, int afdelingsCode) 
+            : base(id, type, datumTijdStart, duurMin, rijksRegisterNrPatient, rijksRegisterNrArts, afdelingsCode) {
+        }
     }
 }
--- ZorgcentrumSln/ZorgcentrumBL/Afspraak/Operatie.cs
+++ ZorgcentrumSln/ZorgcentrumBL/Afspraak/Operatie.cs
@@ -4,5 +4,8 @@ using System.Text;
 
 namespace ZorgcentrumBL.Afspraak {
     public class Operatie : Afspraak {
+        public Operatie(int id, string type, DateTime datumTijdStart, int duurMin, string rijksRegisterNrPatient, string rijksRegisterNrArts, int afdelingsCode) 
+            : base(id, type, datumTijdStart, duurMin, rijksRegisterNrPatient, rijksRegisterNrArts, afdelingsCode) {
+        }
     }
 }
